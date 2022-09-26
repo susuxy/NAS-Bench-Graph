fe73db62ede51d72ecaa40d563c1cfdd4c2fe2a8
susuxy <xingyu.su@intel.com>
2022-09-26 05:03:17 +0000

    organize functions into class

--- main.py
+++ main.py
@@ -1,175 +1,70 @@
-from socketserver import ForkingMixIn
-from nas_bench_graph.readbench import light_read
-from nas_bench_graph.architecture import Arch
-import pandas as pd
-from tqdm import tqdm
-from utils import key2structure
-from sklearn.model_selection import train_test_split
-import numpy as np
-from sklearn.preprocessing import LabelEncoder
-import xgboost
-from sklearn.metrics import r2_score, mean_squared_error
-import matplotlib.pyplot as plt 
-import os
-import logging
 from args_loader import load_args
+from train_models import ML_model
 
 args = load_args()
-
-dataset_name = args.data
-
-bench = light_read(dataset_name)
-
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=logging.INFO)
-logger = logging.getLogger(__name__)
-handler = logging.FileHandler(dataset_name + '.log')
-logger.addHandler(handler)
-with open(dataset_name + '.log', 'w') as f:
-    pass
-
-logger.info(args)
-
-def read_data(file_name):
-    df_file_path = os.path.join('nas_bench_graph', 'save_df', file_name + '.pkl')
-    if os.path.exists(df_file_path):
-        df = pd.read_pickle(df_file_path)
-    else:
-        # contruct dataframe
-        bench = light_read(file_name)
-        print(f"bench information is {bench[list(bench.keys())[0]].keys()}")
-        if args.data_type == 'short':
-            df = pd.DataFrame(columns=['in_0', 'in_1', 'in_2', 'in_3', 'op_0', 
-            'op_1', 'op_2', 'op_3', 'params', 'latency', 'valid_acc', 'test_acc'])
-            for key_idx in tqdm(bench):
-                info = bench[key_idx]
-                structure = key2structure(key_idx)
-                sample = []
-                sample += structure[0]
-                sample += structure[1]
-
-                bench_sample = [info['para'], info['latency'], info['valid_perf'], info['perf']]
-                sample += bench_sample
-
-                df.loc[df.shape[0]] = sample
-        elif args.data_type == 'complete':
-            df = pd.DataFrame(columns=['in_0', 'in_1', 'in_2', 'in_3', 'op_0', 
-            'op_1', 'op_2', 'op_3', 'params', 'latency', 'test_acc'])
-            for key_idx in tqdm(bench):
-                info = bench[key_idx]
-                structure = key2structure(key_idx)
-                sample = []
-                sample += structure[0]
-                sample += structure[1]
-
-                bench_sample = [info['para'], info['latency'], info['perf']]
-                sample += bench_sample
-
-                df.loc[df.shape[0]] = sample
-
-        df.to_pickle(df_file_path)
-    return df
-
-df_total = read_data(dataset_name)
-logger.info(f"total dataframe column names {df_total.columns}")
-logger.info(f"total dataframe size is {df_total.shape}")
+ml_model = ML_model(args)
+category_col = ['in_0', 'in_1', 'in_2', 'in_3', 'op_0', 'op_1', 'op_2', 'op_3']
+ml_model.run(category_col)
 
 
-# data preparation
+# # dataset_name = args.data
 
-def category_label_encode(df, origin_col_names, output_col_names):
-    # category label encoding
-    ops_array = df[origin_col_names].values.reshape(1, -1)
-    ops_array = np.squeeze(ops_array, axis=0)
+# # # bench = light_read(dataset_name)
+
+# # logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=logging.INFO)
+# # logger = logging.getLogger(__name__)
+# # handler = logging.FileHandler(dataset_name + '.log')
+# # logger.addHandler(handler)
+# # with open(dataset_name + '.log', 'w') as f:
+# #     pass
+
+# # logger.info(args)
+
+# # df_total = read_data(dataset_name)
+# # logger.info(f"total dataframe column names {df_total.columns}")
+# # logger.info(f"total dataframe size is {df_total.shape}")
 
 
-    le = LabelEncoder()
-    le.fit(ops_array)
+# # data preparation
 
-    df[output_col_names] = df[origin_col_names].apply(le.transform)
-
-    return df, le
+# if args.encode == 'category':
+#     df_total, le_ops = category_label_encode(df_total, ['op_0', 'op_1', 'op_2', 'op_3'], ['op_0', 'op_1', 'op_2', 'op_3'])
+# elif args.encode == 'one_hot':
+#     one_hot_col = ['in_0', 'in_1', 'in_2', 'in_3', 'op_0', 'op_1', 'op_2', 'op_3']
+#     df_total = one_hot_label_encode(df_total, one_hot_col)
 
 
 
-# one-hot label encoding
-def one_hot_label_encode(df, one_hot_column_names):
-    df = pd.get_dummies(df, prefix = one_hot_column_names, columns=one_hot_column_names)
-    return df
+# # data split
+# col_name_in_op = [col for col in df_total if col.startswith('in') or col.startswith('op')]
+# col_names_x = col_name_in_op + ['params']
 
-if args.encode == 'category':
-    df_total, le_ops = category_label_encode(df_total, ['op_0', 'op_1', 'op_2', 'op_3'], ['op_0', 'op_1', 'op_2', 'op_3'])
-elif args.encode == 'one_hot':
-    one_hot_col = ['in_0', 'in_1', 'in_2', 'in_3', 'op_0', 'op_1', 'op_2', 'op_3']
-    df_total = one_hot_label_encode(df_total, one_hot_col)
-
-
-
-def split_dataset(df, feat_col_name, pred_col_name, test_size, random_seed):
-
-    X = df[feat_col_name]
-    y = df[pred_col_name]
-    return train_test_split(
-        X, y, test_size=test_size, random_state=random_seed
-    )
-
-
-col_name_in_op = [col for col in df_total if col.startswith('in') or col.startswith('op')]
-col_names_x = col_name_in_op + ['params']
-
-# col_names_x = ['in_0', 'in_1', 'in_2', 'in_3', 'op_0_encode', 'op_1_encode', 'op_2_encode', 'op_3_encode', 'params']
-col_names_y = ['test_acc']
-X_train, X_test, y_train, y_test = split_dataset(df_total, col_names_x, col_names_y, 0.2, 13)
-logger.info(f"training data size for X and y is {X_train.shape} and {y_train.shape}")
-logger.info(f"testing data size for X and y is {X_test.shape} and {y_test.shape}")
+# # col_names_x = ['in_0', 'in_1', 'in_2', 'in_3', 'op_0_encode', 'op_1_encode', 'op_2_encode', 'op_3_encode', 'params']
+# col_names_y = ['test_acc']
+# X_train, X_test, y_train, y_test = split_dataset(df_total, col_names_x, col_names_y, 0.2, 13)
+# logger.info(f"training data size for X and y is {X_train.shape} and {y_train.shape}")
+# logger.info(f"testing data size for X and y is {X_test.shape} and {y_test.shape}")
 
 
 
-def model_training(params, X, y):
-    model = xgboost.XGBRegressor(**params)
-    model.fit(X.values, y.values)
-    return model
+
+# # model training and testing
+# params = {
+#     "n_estimators": 500,
+#     "max_depth": 4,
+# }
+# model = model_training(params, X_train, y_train)
 
 
-params = {
-    "n_estimators": 500,
-    "max_depth": 4,
-}
-model = model_training(params, X_train, y_train)
+# r2, mse = model_results(model, X_test.values, y_test)
+# # print([r2, mse])
+# logger.info(f"R2 score {r2:.7f}")
+# logger.info(f"MSE score {mse:.7f}")
 
 
-def model_testing(model, X):
-    y = model.predict(X)
-    return y
 
-def model_results(model, X, y_true):
-    y_pred = model_testing(model, X)
-    r2 = r2_score(y_true, y_pred)
-    mse = mean_squared_error(y_true, y_pred)
-    return r2, mse
-
-r2, mse = model_results(model, X_test.values, y_test)
-# print([r2, mse])
-logger.info(f"R2 score {r2:.7f}")
-logger.info(f"MSE score {mse:.7f}")
-
-
-def plot_feat_importance(model, feat_names, plot_save_path):
-    feat_important = model.feature_importances_
-
-    zipped_list = zip(feat_important, feat_names)
-    sorted_zip_list = sorted(zipped_list, reverse=True)
-    tuples = zip(*sorted_zip_list)
-    feat_important_sort, feat_names_sort = [list(t) for t in tuples]
-    logger.info(f"five most important features {feat_names_sort[:5]}")
-
-    plt.figure(figsize=(16, 9))
-    plt.bar(feat_names, feat_important)
-    plt.title('feature importance')
-    plt.xlabel('feature name')
-    plt.ylabel('importance scores')
-    plt.savefig(plot_save_path)
-
-# all_feats = ['in_0', 'in_1', 'in_2', 'in_3', 'op_0', 'op_1', 'op_2', 'op_3', 'params']
-plot_feat_importance(model, col_names_x, 'importance.png')
+# # plot feature importances
+# # all_feats = ['in_0', 'in_1', 'in_2', 'in_3', 'op_0', 'op_1', 'op_2', 'op_3', 'params']
+# plot_feat_importance(model, col_names_x, 'importance.png')
 
 
---
+++ train_models.py
@@ -0,0 +1,162 @@
+import os
+from tracemalloc import start
+import pandas as pd
+import numpy as np
+from nas_bench_graph.readbench import light_read
+from tqdm import tqdm
+from utils import key2structure
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+import logging
+import xgboost
+from sklearn.metrics import r2_score, mean_squared_error
+import matplotlib.pyplot as plt
+import time
+
+class ML_model:
+    def __init__(self, args):
+        self.args = args
+        self.log_initialize()
+        
+    def log_initialize(self):
+        logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
+        handler = logging.FileHandler(self.args.data + '.log')
+        self.logger.addHandler(handler)
+        with open(self.args.data + '.log', 'w') as f:
+            pass
+        self.logger.info(self.args)
+
+    def run(self, category_col_names):
+        # read data
+        self.read_data()
+
+        # encode category variables
+        if self.args.encode == 'category':
+            self.category_label_encode(category_col_names)
+        elif self.args.encode == 'one_hot':
+            self.one_hot_label_encode(category_col_names)
+        
+        # data split
+        col_names_x = [col for col in self.df if col.startswith('in') or col.startswith('op')]
+        col_names_x = col_names_x + ['params']
+        col_names_y = ['test_acc']
+        self.split_dataset(col_names_x, col_names_y)
+
+        # model training
+        params = {
+            "n_estimators": 500,
+            "max_depth": 4,
+        }
+        start_time = time.time()
+        self.model_training(params)
+        self.logger.info(f"model training time is {time.time() - start_time}")
+
+        # model testing
+        self.model_results()
+
+        # feature importances
+        self.plot_feat_importance(col_names_x)
+
+
+
+    def read_data(self):
+        df_file_path = os.path.join('nas_bench_graph', 'save_df', self.args.data + '.pkl')
+        if os.path.exists(df_file_path):
+            df = pd.read_pickle(df_file_path)
+        else:
+            # contruct dataframe
+            bench = light_read(self.args.data)
+            print(f"bench information is {bench[list(bench.keys())[0]].keys()}")
+            if self.args.data_type == 'short':
+                df = pd.DataFrame(columns=['in_0', 'in_1', 'in_2', 'in_3', 'op_0', 
+                'op_1', 'op_2', 'op_3', 'params', 'latency', 'valid_acc', 'test_acc'])
+                for key_idx in tqdm(bench):
+                    info = bench[key_idx]
+                    structure = key2structure(key_idx)
+                    sample = []
+                    sample += structure[0]
+                    sample += structure[1]
+
+                    bench_sample = [info['para'], info['latency'], info['valid_perf'], info['perf']]
+                    sample += bench_sample
+
+                    df.loc[df.shape[0]] = sample
+            elif self.args.data_type == 'complete':
+                df = pd.DataFrame(columns=['in_0', 'in_1', 'in_2', 'in_3', 'op_0', 
+                'op_1', 'op_2', 'op_3', 'params', 'latency', 'test_acc'])
+                for key_idx in tqdm(bench):
+                    info = bench[key_idx]
+                    structure = key2structure(key_idx)
+                    sample = []
+                    sample += structure[0]
+                    sample += structure[1]
+
+                    bench_sample = [info['para'], info['latency'], info['perf']]
+                    sample += bench_sample
+
+                    df.loc[df.shape[0]] = sample
+
+            df.to_pickle(df_file_path)
+        self.df = df
+        self.logger.info(f"total dataframe column names {df.columns}")
+        self.logger.info(f"total dataframe size is {df.shape}")
+        # return df
+
+    def category_label_encode(self, col_names):
+        # category label encoding
+        ops_array = self.df[col_names].values.reshape(1, -1)
+        ops_array = np.squeeze(ops_array, axis=0)
+
+
+        self.le = LabelEncoder()
+        self.le.fit(ops_array)
+
+        self.df[col_names] = self.df[col_names].apply(self.le.transform)
+
+
+    def one_hot_label_encode(self, col_names):
+        self.df = pd.get_dummies(self.df, prefix = col_names, columns=col_names)
+
+    def split_dataset(self, feat_col_name, pred_col_name, test_size=0.2, random_seed=13):
+
+        X = self.df[feat_col_name]
+        y = self.df[pred_col_name]
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            X, y, test_size=test_size, random_state=random_seed
+        )
+        self.logger.info(f"training data size for X and y is {self.X_train.shape} and {self.y_train.shape}")
+        self.logger.info(f"testing data size for X and y is {self.X_test.shape} and {self.y_test.shape}")
+
+    def model_training(self, params):
+        self.model = xgboost.XGBRegressor(**params)
+        self.model.fit(self.X_train.values, self.y_train.values)
+
+    def model_testing(self):
+        self.y_pred = self.model.predict(self.X_test.values)
+
+    def model_results(self):
+        self.model_testing()
+        r2 = r2_score(self.y_test, self.y_pred)
+        mse = mean_squared_error(self.y_test, self.y_pred)
+        self.logger.info(f"R2 score {r2:.7f}")
+        self.logger.info(f"MSE score {mse:.7f}")
+        self.result = [r2, mse]
+
+    def plot_feat_importance(self, feat_names, plot_save_path='importance.png', feat_num=5):
+        feat_important = self.model.feature_importances_
+
+        zipped_list = zip(feat_important, feat_names)
+        sorted_zip_list = sorted(zipped_list, reverse=True)
+        tuples = zip(*sorted_zip_list)
+        feat_important_sort, feat_names_sort = [list(t) for t in tuples]
+        self.logger.info(f"{feat_num} most important features {feat_names_sort[:feat_num]}")
+
+        plt.figure(figsize=(16, 9))
+        plt.bar(feat_names, feat_important)
+        plt.title('feature importance')
+        plt.xlabel('feature name')
+        plt.ylabel('importance scores')
+        plt.xticks(rotation='90')
+        plt.savefig(plot_save_path)
+
